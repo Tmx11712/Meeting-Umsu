eb4b8c533202a35fda868d2b037459702eead253
aryaprtama0417-spec <aryaprtama0417@gmail.com>
2026-08-20 10:05:47 +0700

    fix(meeting): add SerializesModels and Dispatchable traits to background job

--- meeting/app/Jobs/GenerateMeetingMinuteJob.php
+++ meeting/app/Jobs/GenerateMeetingMinuteJob.php
@@ -4,13 +4,16 @@ namespace App\Jobs;
 
 use App\Actions\Meetings\GenerateMeetingMinuteAction;
 use App\Models\Meeting;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
 class GenerateMeetingMinuteJob implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * Berikan waktu maksimal 5 menit untuk job ini sebelum dianggap timeout.
