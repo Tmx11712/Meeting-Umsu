02ea8a94a9b2c0a17d18a3b064fcfd0a8fd67a7b
aryaprtama0417-spec <aryaprtama0417@gmail.com>
2026-09-16 15:39:12 +0700

    fix: Populate MasterDataSeeder with initial records

--- meeting/database/seeders/MasterDataSeeder.php
+++ meeting/database/seeders/MasterDataSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\MeetingRoom;
+use App\Models\MeetingType;
+
 class MasterDataSeeder extends Seeder
 {
     /**
@@ -12,6 +15,26 @@ class MasterDataSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+            'Rapat internal',
+            'Rapat Resmi',
+            'Rapat Rutin',
+            'Rapat Koordinasi',
+            'Rapat Persiapan'
+        ];
+
+        foreach ($types as $type) {
+            MeetingType::firstOrCreate(['name' => $type]);
+        }
+
+        $rooms = [
+            'Ruangan VIP lantai 2 gedung rektor',
+            'Ruangan Rapat Lantai 3 Gedung Rektor',
+            'ruangan Work of fame'
+        ];
+
+        foreach ($rooms as $room) {
+            MeetingRoom::firstOrCreate(['name' => $room]);
+        }
     }
 }
