3f98fba61b04466ed385ed615932c858c193db72
aryaprtama0417-spec <aryaprtama0417@gmail.com>
2026-09-16 15:49:56 +0700

    fix: Hapus duplikasi use Request di controller Master Data

--- meeting/app/Http/Controllers/Configuration/MeetingRoomController.php
+++ meeting/app/Http/Controllers/Configuration/MeetingRoomController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\MeetingRoom;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class MeetingRoomController extends Controller
--- meeting/app/Http/Controllers/Configuration/MeetingTypeController.php
+++ meeting/app/Http/Controllers/Configuration/MeetingTypeController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\MeetingType;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class MeetingTypeController extends Controller
